ff3be23980d00b417c4670474fa82d03b0d87ab2
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2023-08-18 09:51:19 -0500

    Update clam-rk-update-scan.sh
    Signed-off-by: Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>

--- anti-virus/clam-rk-update-scan.sh
+++ anti-virus/clam-rk-update-scan.sh
@@ -17,3 +17,8 @@ rkhunter --propupd;
 
 #check
 rkhunter --check;
+
+##################################################
+
+# once you save the above script then make it executable with this command
+#chmod +x clam-rk-update-scan.sh
